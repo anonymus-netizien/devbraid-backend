e7d11ae720561fdd564e3a532b03641a55e8db4d
anonymus-netizien <kollururamchandarrao@gmail.com>
2026-07-22 00:50:05 +0530

    feat(scaffold): initialize spring boot 4.1.0 backend skeleton

---
+++ src/main/java/com/devbraid/DevbraidBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devbraid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevbraidBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevbraidBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devbraid/DevbraidBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbraid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevbraidBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
